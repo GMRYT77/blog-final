fcfb20c07c65c6df2626cfe5a59d210aa83f972d
GMRYT77 <rajaryan16218@gmail.com>
2023-05-14 15:34:30 +0530

    3

--- components/Quote.js
+++ components/Quote.js
@@ -5,15 +5,15 @@ const Quote = () => {
   return (
     <section className="w-full bg-t">
       <div className="cont py-16 flex justify-evenly text-white">
-        <div className="flex">
-          <ImQuotesLeft className="text-[4rem] -top-5 " />
+        <div className="flex relative">
+          <ImQuotesLeft className="absolute text-[4rem] -top-8 left-0" />
         </div>
         <p className=" leading-[50px] line-clamp-3 text-4xl zilla-i text-center  max-w-[800px]">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
           autem in saepe laboriosam. Doloribus in sequi rerum id tenetur porro!
         </p>
-        <div className="flex items-end">
-          <ImQuotesRight className="text-[4rem]" />
+        <div className="flex relative items-end">
+          <ImQuotesRight className="absolute text-[4rem] -bottom-8 right-0" />
         </div>
       </div>
     </section>
