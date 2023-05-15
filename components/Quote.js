855dbebdbf8291472587070162fc7619240208da
GMRYT77 <rajaryan16218@gmail.com>
2023-05-15 11:16:11 +0530

    9

--- components/Quote.js
+++ components/Quote.js
@@ -3,18 +3,14 @@ import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
 
 const Quote = () => {
   return (
-    <section className="w-full bg-t">
-      <div className="cont py-16 flex justify-evenly text-white">
-        <div className="flex relative">
-          <ImQuotesLeft className="absolute text-[4rem] -top-8 left-0" />
-        </div>
-        <p className=" leading-[50px] line-clamp-3 text-4xl zilla-i text-center  max-w-[800px]">
+    <section className="relative w-full bg-t sec flex items-center">
+      <div className="relative w-full max-w-[900px] px-2 xs:px-6 sm:px-8 md:px-14 py-28 text-white ">
+        <ImQuotesLeft className="absolute text-[3rem] md:text-[4rem] top-7 left-8" />
+        <p className=" line-clamp-3 text-xl xs:text-2xl sm:text-3xl md:text-4xl zilla-i text-center  max-w-[800px]">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
           autem in saepe laboriosam. Doloribus in sequi rerum id tenetur porro!
         </p>
-        <div className="flex relative items-end">
-          <ImQuotesRight className="absolute text-[4rem] -bottom-8 right-0" />
-        </div>
+        <ImQuotesRight className="absolute text-[3rem] md:text-[4rem] bottom-7 right-8" />
       </div>
     </section>
   );
